f964750388e5ff0ff33752607ed14e8521cef178
kludgebot <akosialexpo@gmail.com>
2023-07-06 23:20:58 -0700

    add: Dogs service

---
+++ dogs.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const fetchDogs = async () => {
+  const response = await axios.get('https://api.thedogapi.com/v1/breeds?limit=5', {}, {
+    headers: {
+        'content-type': 'application/json',
+    }
+  });
+
+  return response.data;
+};
+
+export default fetchDogs;
