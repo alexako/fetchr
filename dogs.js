1674054204d85e5e205f5ef7bde8fecdae69ee2f
kludgebot <akosialexpo@gmail.com>
2023-07-06 23:42:42 -0700

    chore: Refactor and cleanup

--- dogs.js
+++ dogs.js
@@ -1,11 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
 const fetchDogs = async () => {
-  const response = await axios.get('https://api.thedogapi.com/v1/breeds?limit=5', {}, {
-    headers: {
-        'content-type': 'application/json',
+  const response = await axios.get(
+    "https://api.thedogapi.com/v1/breeds?limit=5",
+    {},
+    {
+      headers: {
+        "content-type": "application/json",
+      },
     }
-  });
+  );
 
   return response.data;
 };
