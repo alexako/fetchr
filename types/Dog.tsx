212e0f814f08f7f77a93791832fde59f8bdf752f
kludgebot <akosialexpo@gmail.com>
2023-07-06 23:37:43 -0700

    add: Dog type

---
+++ types/Dog.tsx
@@ -0,0 +1,20 @@
+export type Dog = {
+    id: number;
+    bred_for: string;
+    breed_group: string;
+    height: {
+        imperial: string;
+        metric: string;
+    },
+    image: {
+        url: string;
+    },
+    life_span: string;
+    name: string;
+    origin: string;
+    temperament: string;
+    weight: {
+        imperial: string;
+        metric: string;
+    },
+}
