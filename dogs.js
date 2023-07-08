9bc66f0ae3fdf6eccb8190ec7bb691a40a3e7f45
kludgebot <akosialexpo@gmail.com>
2023-07-08 01:31:20 -0700

    feat: Pagination

--- dogs.js
+++ dogs.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const fetchDogs = async (page) => {
   const response = await axios.get(
-    `https://api.thedogapi.com/v1/breeds?limit=15&${page},`,
+    `https://api.thedogapi.com/v1/breeds?limit=7&page=${page},`,
     {},
     {
       headers: {
